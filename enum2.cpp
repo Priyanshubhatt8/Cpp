42213fdb2aefc6ac7db413ddefff997779bb55be
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-07 23:06:15 +0530

    Add files via upload

---
+++ enum2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//enum{breakfast,lunch,dinner};
+int main(){
+//int a=7;
+//int &b=a;
+float e=1.21;
+cout<<int(e);
+   // cout<<e;
+    
+    
+    
+
+
+    return 0;
+}
