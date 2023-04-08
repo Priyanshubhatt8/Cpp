abbcc48f4baff92424e70e210d3afee02900b459
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-08 12:13:39 +0530

    Add files via upload

---
+++ functionnoargumentsnoreturns.cpp
@@ -0,0 +1,18 @@
+
+//FOUR WAYS OF FUNCTION --1st(NO arguments No returns)
+#include<iostream>
+using namespace std;
+void add(void);
+int main(){
+add();
+
+return 0;
+}
+void add(void){
+
+int a,b,sum;
+cin>>a>>b;
+sum=a+b;
+cout<<sum;
+
+}
