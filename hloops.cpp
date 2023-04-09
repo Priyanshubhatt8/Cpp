9c1d09351eb35c59c59386dae125bdaf13cc9910
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-10 00:18:07 +0530

    Add files via upload

---
+++ hloops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+//LOOPS
+//Syntax for for loop
+/*for(initialization; condition; update;){
+  loop body(in c++ code)
+}*/
+
+for(int i=0;i<=10;i++){
+cout<<i*6<<endl;
+
+}
+
+
+
+
+    return 0;
+}
