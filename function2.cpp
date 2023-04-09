c15c59c6c1410a6bbd0e55745c45631e0691410d
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-10 00:17:01 +0530

    Add files via upload

---
+++ function2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b){
+    int c=a+b;
+    return c;
+
+
+}
+int main(){
+
+cout<<add(6,9)<<endl;
+
+cout<<add(28,438);
+
+
+    return 0;
+}
