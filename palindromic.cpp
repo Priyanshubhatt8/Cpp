c0573619f47f513e4f5fcfb7633f11359fa79dbd
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-17 10:47:32 +0530

    Add files via upload

---
+++ palindromic.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+for(int j=1;j<=n-i;j++){
+    cout<<" ";}
+    for(int c=1;c<=i;c++){
+    cout<<c;   
+       
+
+}cout<<endl;
+
+
+}
+
+    return 0;
+}
